dec633eeb79276aca8edaf9be12e9beab4d4b4e4
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-12-05 15:09:23 -0800

    first pass at api call for data generations

--- api.py
+++ api.py
@@ -1,41 +1,153 @@
 import asyncio
 import json
 import os
+import time
 
 import aiohttp
+import numpy as np
 from dotenv import load_dotenv
 
 
-async def call_openai(data):
-    load_dotenv()
-    api_key = os.getenv("OPENAI_API_KEY")
-    print(api_key)
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}",
-    }
+class OpenAI_API:
+    RETRY_ATTEMPTS = 5
+    RETRY_DELAY = 2
+    SAMPLES = 10
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, data=json.dumps(data), headers=headers) as resp:
-            response = await resp.text()
-            print(response)
+    def __init__(self):
+        self.session = None
+        self.throughput = np.zeros(self.SAMPLES)
+        self.throughput_index = 0
+
+    async def send_prompt(self, prompt, index, results_queue, inputs_queue):
+        base_url = os.getenv("OPENAI_API_BASE")
+        url = f"{base_url}/chat/completions"
+        headers = self.get_headers()
+        data = self.get_data(prompt)
+        for retry in range(self.RETRY_ATTEMPTS):
+            try:
+                start_time = time.time()
+                result, response_text = await self.post_request(url, headers, data)
+                end_time = time.time()
+                self.update_throughput(end_time - start_time)
+                if self.is_special_response(response_text):
+                    continue
+                else:
+                    await self.is_normal_response(
+                        index, result, prompt, results_queue, inputs_queue
+                    )
+                break
+            except Exception as e:
+                print(f"Error sending prompt {index} (retry {retry + 1}):", e)
+                if retry == self.RETRY_ATTEMPTS - 1:
+                    print(
+                        f"Failed to process prompt {index} after {self.RETRY_ATTEMPTS} retries."
+                    )
+                    return (index, None)
+                await asyncio.sleep(self.RETRY_DELAY ** (retry + 1))
+
+    def get_headers(self):
+        return {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}",
+        }
+
+    def get_data(self, prompt):
+        return {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": prompt["prompt"]}],
+        }
+
+    async def post_request(self, url, headers, data):
+        async with self.session.post(url, headers=headers, json=data) as response:
+            result = await response.json()
+            return result, json.dumps(result)
+
+    def is_special_response(self, response_text):
+        return any(
+            phrase in response_text
+            for phrase in [
+                "%assistant%",
+                "%I'm sorry%",
+                "%I cannot%",
+                "%language model%",
+            ]
+        )
+
+    async def is_normal_response(
+        self, index, result, prompt, results_queue, inputs_queue
+    ):
+        print(f"Model output for prompt {index}:", json.dumps(result))
+        await results_queue.put((index, result))
+        await inputs_queue.put((index, prompt))
+        return (index, result)
+
+    def update_throughput(self, elapsed_time):
+        self.throughput[self.throughput_index] = 1 / elapsed_time
+        self.throughput_index = (self.throughput_index + 1) % self.SAMPLES
+
+    async def process_prompts(self, prompt_generator):
+        results_queue = asyncio.Queue()
+        inputs_queue = asyncio.Queue()
+
+        asyncio.create_task(
+            self.write_to_file(results_queue, "finished/complete.jsonl")
+        )
+        asyncio.create_task(
+            self.write_to_file(inputs_queue, "finished/indexed_inputs.jsonl")
+        )
+
+        async with aiohttp.ClientSession() as self.session:
+            batch = []
+            for index, prompt in enumerate(prompt_generator):
+                batch.append((index, prompt))
+                if len(batch) >= self.SAMPLES:
+                    await self.process_batch(batch, results_queue, inputs_queue)
+                    batch = []
+            if batch:
+                await self.process_batch(batch, results_queue, inputs_queue)
+
+        await results_queue.put(None)
+        await inputs_queue.put(None)
+
+    async def write_to_file(self, queue, file_name):
+        with open(file_name, "w") as f:
+            while True:
+                item = await queue.get()
+                if item is None:
+                    break
+                f.write(json.dumps(item) + "\n")  # new line because jsonl
+
+    async def process_batch(self, batch, results_queue, inputs_queue):
+        tasks = [
+            self.send_prompt(prompt, index, results_queue, inputs_queue)
+            for index, prompt in batch
+        ]
+        return await asyncio.gather(*tasks)
+
+
+def ask_for_generations():
+    while True:
+        try:
+            generations = int(input("How many generations would you like to perform? "))
+            return generations
+        except ValueError as e:
+            print("Invalid input, please enter an integer.")
+
+
+def prompt_generator(generations):
+    with open("processed/preapi.jsonl", "r") as f:
+        prompts = [json.loads(line) for line in f][:generations]
+        for prompt in prompts:
+            yield prompt
 
 
 def main():
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {
-                "role": "system",
-                "content": "You are a council of experts. Two of you are experts in the field, and one of you is a novice that is very inquisitive. Ensure the novice asks at least 1 question. Ensure one of the experts answers the question.",
-            },
-            {"role": "user", "content": "What is the theory of relativity?"},
-        ],
-    }
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(call_openai(data))
+    api = OpenAI_API()
+    generations = ask_for_generations()
+    try:
+        asyncio.run(api.process_prompts(prompt_generator(generations)))
+    except Exception as e:
+        print("An error occurred: ", e)
 
 
 if __name__ == "__main__":
