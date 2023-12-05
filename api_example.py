65745349043a06ab13ffd9e0b7e357b83a503c88
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-12-05 11:38:39 -0800

    rename api file to make room for functional file

---
+++ api_example.py
@@ -0,0 +1,42 @@
+import asyncio
+import json
+import os
+
+import aiohttp
+from dotenv import load_dotenv
+
+
+async def call_openai(data):
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    print(api_key)
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=json.dumps(data), headers=headers) as resp:
+            response = await resp.text()
+            print(response)
+
+
+def main():
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a council of experts. Two of you are experts in the field, and one of you is a novice that is very inquisitive. Ensure the novice asks at least 1 question. Ensure one of the experts answers the question.",
+            },
+            {"role": "user", "content": "What is the theory of relativity?"},
+        ],
+    }
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(call_openai(data))
+
+
+if __name__ == "__main__":
+    main()
